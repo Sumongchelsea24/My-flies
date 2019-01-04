11d0c7b8bcc5cb37a79f7949cd29df167f1025ff
Sujan Chelsea <sujanchelsea24@gmail.com>
2019-01-04 08:46:33 +0545

    added static block code

---
+++ StaticTest.java
@@ -0,0 +1,23 @@
+public class StaticTest{
+    static {
+        int i=10;
+        System.out.println("static block called");
+    }
+    StaticTest(){
+        System.out.println("Hi i am a constructor");
+    }
+    public static void main(String[] args){
+        StaticTest st= new StaticTest();
+        System.out.println(StaticTest2.testMethod());
+
+    }
+}
+class StaticTest2{
+    static String name;
+    static{
+         name="Rameshwor Yadav"; //static block
+    }
+    public static String testMethod(){
+        return name;
+    }
+}
